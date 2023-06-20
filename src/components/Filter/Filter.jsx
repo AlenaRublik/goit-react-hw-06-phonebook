1af83bf48df0f207d8ecaf3863def493542cf3e6
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-06-21 00:27:23 +0300

    bugfix filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,15 +2,16 @@ import { Label, Input } from './Filter.styled';
 import { useDispatch } from 'react-redux';
 import { updateFilterValue } from 'redux/filterSlice';
 
-export const Filter = () => { 
-    const dispatch = useDispatch();
-  <Label>
+export const Filter = () => {
+  const dispatch = useDispatch();
+  return (
+    <Label>
       Filter by name or number
       <Input
         type="text"
         name="filter"
         onChange={e => dispatch(updateFilterValue(e.target.value))}
       />
-  </Label>
+    </Label>
+  );
 };
-
